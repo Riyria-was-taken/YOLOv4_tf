f5e25b84cc42ddd2166dcf425fad74a9bcb5fbaa
Paweł <pawel.anikiel@gmail.com>
2020-12-06 17:47:10 +0100

    functions

--- img.py
+++ img.py
@@ -1,15 +1,13 @@
 import tensorflow as tf
 import cv2
 
-path = 'birb.jpg'
-size = 416
+def read_img(path, size):
+	img = tf.image.decode_image(open(path, 'rb').read(), channels=3)
+	pixels = cv2.cvtColor(img.numpy(), cv2.COLOR_RGB2BGR)
+	img = tf.image.resize(img, (size, size)) / 255
+	data = tf.data.Dataset.from_tensors(img)
+	return (pixels, data)
 
-img = tf.image.decode_image(open(path, 'rb').read(), channels=3)
-pix = img.numpy()
-
-img = tf.image.resize(img, (size, size)) / 255
-# do detection
-
-pix = cv2.cvtColor(pix, cv2.COLOR_RGB2BGR)
-cv2.imshow('A birb', pix)
-cv2.waitKey(0)
+def draw_img(pixels, boxes, scores, classes):
+	cv2.imshow('Image', pixels)
+	cv2.waitKey(0)
