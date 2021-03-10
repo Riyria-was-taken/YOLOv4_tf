480bb46da4a010a8b5eba3a787d350e9d7d6f25e
Kacper Kluk <kk406184@students.mimuw.edu.pl>
2021-03-10 09:23:11 +0100

    clean up train.py

--- train.py
+++ train.py
@@ -13,8 +13,9 @@ def calc_loss(gt_boxes, gt_labels, preds):
     tot_box_loss = 0
     tot_obj_loss = 0
     tot_cls_loss = 0
-    for i, preds in enumerate(output):
-        s = preds.shape
+    outputs = utils.decode_preds(preds)
+    for i in range(len(outputs)):
+        s = outputs[i].shape
         gw, gh = s[1:3]
         stride_x = 1 / gw
         stride_y = 1 / gh
@@ -48,21 +49,11 @@ def calc_loss(gt_boxes, gt_labels, preds):
             for ir in good:
                 truth_mask[0, iy, ix, ir] = 1
 
-                data = preds[0, iy, ix, (d // 3) * ir : (d // 3) * (ir + 1)]
-
-                dx, dy, dw, dh = data[:4]
-                x = (utils.sigmoid(dx) * scales[i] - 0.5 * (scales[i] - 1) + ix) * stride_x
-                y = (utils.sigmoid(dy) * scales[i] - 0.5 * (scales[i] - 1) + iy) * stride_y
-
-                w, h = anchor_sizes[i][ir]
-                w /= 608
-                h /= 608
-                w *= math.exp(dw)
-                h *= math.exp(dh)
-                pred_box = [x, y, w, h]
+                data = outputs[i][0, iy, ix, (d // 3) * ir : (d // 3) * (ir + 1)]
+                pred_box = data[ : 4]
+                cls_preds = data[5 : ]
 
                 box_loss += 1 - utils.calc_gious(gt_box[:4], pred_box)
-                cls_preds = [utils.sigmoid(x) for x in data[5:]]
                 for ic in range(len(cls_preds)):
                     pred = cls_preds[ic]
                     truth = 1 if gt_cls == ic else 0
@@ -70,20 +61,10 @@ def calc_loss(gt_boxes, gt_labels, preds):
 
         inv_truth_mask = 1 - truth_mask
         obj_loss = 0
-        ix = tf.cast(tf.tile(tf.expand_dims(tf.range(gw), axis=0), [gw, 1]), tf.float32)
-        iy = tf.cast(tf.tile(tf.expand_dims(tf.range(gw), axis=1), [1, gh]), tf.float32)
         for ir in range(3):
-            data = preds[:, :, :, (d // 3) * ir : (d // 3) * (ir + 1)]
-            dx = data[:, :, :, 0]
-            dy = data[:, :, :, 1]
-            dw = data[:, :, :, 2]
-            dh = data[:, :, :, 3]
-            x = (utils.sigmoid(dx) * scales[i] - 0.5 * (scales[i] - 1) + ix) * stride_x
-            y = (utils.sigmoid(dy) * scales[i] - 0.5 * (scales[i] - 1) + iy) * stride_y
-            w = tf.math.exp(dw) * anchor_sizes[i][ir][0] / 608
-            h = tf.math.exp(dh) * anchor_sizes[i][ir][1] / 608
-            pred_boxes = tf.stack([x, y, w, h], axis=3)
-            objectness = utils.sigmoid(data[:, :, :, 4])
+            data = outputs[i][..., (d // 3) * ir : (d // 3) * (ir + 1)]
+            pred_boxes = data[..., : 4]
+            objectness = data[..., 4]
 
             best_ious = tf.zeros([1, gw, gh])
             for gt_box in gt_boxes:
@@ -98,7 +79,6 @@ def calc_loss(gt_boxes, gt_labels, preds):
         tot_box_loss += box_loss
         tot_obj_loss += obj_loss
         tot_cls_loss += cls_loss
-        #print(box_loss, obj_loss, cls_loss)
 
     return tot_box_loss, tot_obj_loss, tot_cls_loss
 
